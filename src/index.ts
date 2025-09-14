808c6e646627b604a88199d587671c301f12029d
sangeetKumar <ps8711093@gmail.com>
2025-09-14 09:16:53 +0000

    code updated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-const express =require('express')
-
+import express from "express";
+import router from "./router/router.js";
+import mongoose, { mongo } from 'mongoose';
 const app=express();
+
+ mongoose.connect("mongodb://localhost:27017")
+.then(()=>{console.log("DB connected successfully")})
+.catch((err)=> console.log("error in db connection ",err))
+app.use(express.json())
+
+
+app.use("/api",router)
+
 
 app.listen(4000,()=>{
     console.log("server is running at port : ",4000)
